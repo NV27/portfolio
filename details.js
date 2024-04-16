945509aa783bd3e0137bdbaf387e98a14c9188f5
Louis <louisprotano@hotmail.com>
2024-04-16 18:35:56 +0100

    Working on mobile view

--- details.js
+++ details.js
@@ -1,16 +1,35 @@
 
 myModel = document.querySelector(".model");
 closeButton = document.querySelector(".close-button");
+c1 = document.getElementById("c1-content");
 c2 = document.getElementById("c2-content");
+c3 = document.getElementById("c3-content");
 
 var modelOpen = 0;
 
-var title = "bloop";
+var title = "Helenite";
 var image = "images/art/ghastlies/helenite.png";
 
+var images = [
+    "images/art/ghastlies/helenite.png",
+    "images/art/ghastlies/liberty-anim.gif",
+    "images/art/popsicle-pals/pp-eggplant.png",
+    "images/games/neon-parasite-cover.png"
+]
+
 c2.innerHTML = "";
-for(i=0;i<3;i++){
+for(i=0;i<4;i++){
     c2.innerHTML += 
+    `
+    <div class="display-box">
+        <h3>${title}</h3>
+        <button class="expander e3 ePic"><img class="base-image" src=${images[i]}></button>
+    </div>
+    `
+}
+
+for(i=0;i<1;i++){
+    c3.innerHTML += 
     `
     <div class="display-box">
         <h3>${title}</h3>

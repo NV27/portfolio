c1f6f358ad20aa1711e0338fd79d7e89f7eea313
Louis <louisprotano@hotmail.com>
2024-04-16 16:58:54 +0100

    working on implemeting js content

--- details.js
+++ details.js
@@ -1,10 +1,25 @@
-e0 = document.querySelector(".ePic");
+
 myModel = document.querySelector(".model");
 closeButton = document.querySelector(".close-button");
+c2 = document.getElementById("c2-content");
 
 var modelOpen = 0;
 
+var title = "bloop";
+var image = "images/art/ghastlies/helenite.png";
 
+c2.innerHTML = "";
+for(i=0;i<3;i++){
+    c2.innerHTML += 
+    `
+    <div class="display-box">
+        <h3>${title}</h3>
+        <button class="expander e3 ePic"><img class="base-image" src=${image}></button>
+    </div>
+    `
+}
+
+e0 = document.querySelector(".ePic");
 
 e0.addEventListener("click", () => {
     if (modelOpen == 0){

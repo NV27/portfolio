15d4a2b9ac2c4dfc72f4b227260efe5678836895
Louis <louisprotano@hotmail.com>
2024-04-17 17:22:50 +0100

    making sure JSON injection works

--- details.js
+++ details.js
@@ -1,7 +1,7 @@
 
 const myModel = document.querySelector(".model");
+const modelTop = document.querySelector(".model-top");
 const modelContent = document.querySelector(".model-content");
-const closeButton = document.querySelector(".close-button");
 const c1 = document.getElementById("c1-content");
 const c2 = document.getElementById("c2-content");
 const c3 = document.getElementById("c3-content");
@@ -9,13 +9,7 @@ const c3 = document.getElementById("c3-content");
 
 
 let modelOpen = 0;
-closeButton.addEventListener("click", () => {
-    if (modelOpen == 1){
-        modelOpen = 0;
-        
-        myModel.classList.add("hidden");
-    }
-})
+
 
 //Goes after data is loaded
 function addModelOpen(){
@@ -68,10 +62,23 @@ function populateModel(key){
         })
 
     
-
+    modelTop.innerHTML = `
+        <div id="model-date">${myInfo.releaseDate}</div>
+        <div id="model-title">${myInfo.title}</div>
+        <button class="close-button">x</button>
+        `
     // modelContent.innerHTML = `
     // <div>INFOROMATION AND PICTURES AND STUFF</div>
     // `;
+
+    document.querySelector(".close-button").addEventListener("click", () => {
+        if (modelOpen == 1){
+            modelOpen = 0;
+            
+            myModel.classList.add("hidden");
+        }
+    })
+
     })
 
 }
@@ -107,53 +114,3 @@ function loadJson(){
 }
 
 loadJson();
-
-//TEST DATA
-// var title = "Helenite";
-// var image = "images/art/ghastlies/helenite.png";
-
-// var images = [
-//     "images/art/ghastlies/helenite.png",
-//     "images/art/ghastlies/liberty-anim.gif",
-//     "images/art/popsicle-pals/pp-eggplant.png",
-//     "images/games/neon-parasite-cover.png"
-// ]
-
-// var items = [
-//     {key: "0", title: "blueberry", image: "images/art/popsicle-pals/pp-blueberry.png"},
-//     {key: "1", title: "lime", image: "images/art/popsicle-pals/pp-lime.png"}
-// ]
-
-// c1.innerHTML = "";
-// for(i=0;i<items.length;i++){
-//     c1.innerHTML += 
-//     `
-//     <div class="display-box">
-//         <h3>${items[i].title}</h3>
-//         <button class="expander e3 ePic"><img class="base-image" src=${items[i].image}></button>
-//     </div>
-//     `
-// }
-
-// console.log("c1.innerHTML");
-
-// c2.innerHTML = "";
-// for(i=0;i<4;i++){
-//     c2.innerHTML += 
-//     `
-//     <div class="display-box">
-//         <h3>${title}</h3>
-//         <button class="expander e3 ePic"><img class="base-image" src=${images[i]}></button>
-//     </div>
-//     `
-// }
-
-// for(i=0;i<1;i++){
-//     c3.innerHTML += 
-//     `
-//     <div class="display-box">
-//         <h3>${title}</h3>
-//         <button class="expander e3 ePic"><img class="base-image" src=${image}></button>
-//     </div>
-//     `
-// }
